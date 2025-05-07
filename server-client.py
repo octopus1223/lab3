c0acdc9c4548b5aa5a45249d78a135d556718250
octopus1223 <2168487918@qq.com>
2025-05-07 21:55:36 +0800

    commit

--- server-client.py
+++ server-client.py
@@ -15,3 +15,25 @@ class TupleSpaceServer:
             "errors": 0
         }
         self.last_report_time = datetime.now()
+    
+    def Start(self):
+        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+            s.blind(('0.0.0.0', self.port))
+            s.listen()
+            print(f"Server listening on the port{self.port}")
+
+            while True:
+                conn, addr = s.accept()
+                self.status['total_clents'] += 1
+                client_threads = threading.Thread(
+                    target = self.handle_client,
+                    args = (conn, addr)
+                )
+                client_threads.start()
+
+                if datatime.now() - self.last_report_time >= timedelta(seconds = 10):
+                    self.report_status()
+                    self.last_report_time = detertime.now()
+                
+
+            
