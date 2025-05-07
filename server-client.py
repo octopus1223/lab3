da6dcff2896c10514b435dc37d4be230484cd695
octopus1223 <2168487918@qq.com>
2025-05-07 21:32:43 +0800

    commit

--- server-client.py
+++ server-client.py
@@ -2,3 +2,16 @@ import socket
 import threading
 from datetime import datetime, timedelta
 
+class TupleSpaceServer:
+    def __init__(self,port):
+        self.port = port
+        self.tuple_space = ()
+        self.lock = threading.Lock()
+        self.status = {
+            "total_clients": 0,
+            "total_operations": 0,
+            "read": 0,
+            "put": 0,
+            "errors": 0
+        }
+        self.last_report_time = datetime.now()
