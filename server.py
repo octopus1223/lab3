6ce4c87d4358776f1e04c2aef68cc4f327e98ba4
octopus1223 <2168487918@qq.com>
2025-05-08 18:18:26 +0800

    commmit

--- server.py
+++ server.py
@@ -20,10 +20,10 @@ class TupleSpaceServer:
         self.last_report_time = datetime.now()
     
     #define the main loop of server
-    def Start(self):
+    def start(self):
         #Create and configure a socket 
         with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-            s.blind(('0.0.0.0', self.port))
+            s.bind(('0.0.0.0', self.port))
             s.listen()
             print(f"Server listening on the port{self.port}")
 
@@ -37,7 +37,7 @@ class TupleSpaceServer:
                 client_threads.start()
 
                 if datetime.now() - self.last_report_time >= timedelta(seconds = 10):
-                    self.report_status()
+                    self.report_stats()
                     self.last_report_time = datetime.now()
     #Use the client handling function
     def handle_client(self,conn,addr):
@@ -56,56 +56,58 @@ class TupleSpaceServer:
                     print(f"Error with client{addr}:{e}")
                     break
         print(f"Client{addr} disconnected")
-        def process_request(self,request):
-            try:
-                size = int(request[:3])
-                cmd = request[4]
-                remaining = request[5:].strip()
 
-                self.status['total_ops'] += 1
+    def process_request(self, request):
+        try:
+            size = int(request[:3])
+            cmd = request[4]
+            remaining = request[5:].strip()
 
-                if cmd == "R":
-                    self.status['read'] += 1
-                    key = remaining
-                    with self.lock:
-                        if key in self.tuple_space:
-                            value = self.tuple_space.pop(key)
-                            return f"{len(f'OK({key},{value})removed')+3:03d}OK({key},{value})read"
-                        else:
-                            self.status["errors"]+= 1
-                            return f"{len(f'ERR{key}does not exist')+3:03d}ERR{key} does not exist"
-                elif cmd == "G":
-                    self.stats["get"] += 1
-                    key = remaining
-                    with self.lock:
-                        if key in self.tuple_space:
-                            value = self.tuple_space.pop(key)
-                            return f"{len(f'OK({key},{value})removed')+3:03d}OK{key} does not exist"
-                        else:
-                            self.stats['errors']+= 1
-                            return f"{len(f'ERR{key}does not exist')+3:03d}ERR{key}does not exist"
-                elif cmd == 'P':
-                    self.stats['put'] += 1
-                    parts = remaining.split(maxsplit = 1)
-                    if len(parts)<2:
-                        self.stats["errors"]+=1
-                        return "005 ERR size exceed"
-                    
-                    key,value = parts
-                    if len(key) + len(value) >970:
+            self.status['total_ops'] += 1
+
+            if cmd == "R":
+                self.status['read'] += 1
+                key = remaining
+                with self.lock:
+                    if key in self.tuple_space:
+                        value = self.tuple_space[key]
+                        return f"{len(f'OK({key},{value})removed') + 3:03d}OK({key},{value})read"
+                    else:
+                        self.status["errors"] += 1
+                        return f"{len(f'ERR{key}does not exist') + 3:03d}ERR{key} does not exist"
+            elif cmd == "G":
+                self.status['get'] += 1
+                key = remaining
+                with self.lock:
+                    if key in self.tuple_space:
+                        value = self.tuple_space.pop(key)
+                        return f"{len(f'OK({key},{value})removed') + 3:03d}OK{key} does not exist"
+                    else:
+                        self.status['errors'] += 1
+                        return f"{len(f'ERR{key}does not exist') + 3:03d}ERR{key}does not exist"
+            elif cmd == 'P':
+                self.status['put'] += 1
+                parts = remaining.split(maxsplit=1)
+                if len(parts) < 2:
+                    self.status["errors"] += 1
+                    return "005 ERR size exceed"
+
+                key, value = parts
+                if len(key) + len(value) > 970:
+                    self.status['errors'] += 1
+                    return "005 ERR size exceed"
+
+                with self.lock:
+                    if key in self.tuple_space:
                         self.stats['errors'] += 1
-                        return "005 ERR size exceed"
-                    
-                    with self.lock:
-                        if key in self.tuple_space:
-                            self.stats['errors']+= 1
-                            return f"{len(f'ERR{key}already exists')+3:03d}ERR{key} already exists"
-                        else:
-                            self.tuple_space[key] = value
-                            return f"{len(f'OK({key},{value})added')+3:03d} OK({key},{value})added"
-            except Exception as e :
-                self.stats["errors"] += 1
-                return "005 ERR invalid request"
+                        return f"{len(f'ERR{key}already exists') + 3:03d}ERR{key} already exists"
+                    else:
+                        self.tuple_space[key] = value
+                        return f"{len(f'OK({key},{value})added') + 3:03d} OK({key},{value})added"
+        except Exception as e:
+            self.stats["errors"] += 1
+            return "005 ERR invalid request"
+
 
     def report_stats(self):
         with self.lock:
@@ -119,10 +121,10 @@ class TupleSpaceServer:
             print(f"Avg tuple size :{avg_tuple_size:.2f}")
             print(f"Avg key size:{avg_key_size:.2f}")
             print(f"Avg value size:{avg_value_size:.2f}")
-            print(f"total clients:{self.stats['total_clients']}")
-            print(f"total operations:{self.stats['total_operations']}")
-            print(f"READS:{self.stats['read']}")
-            print(f"GETS:{self.stats['get']}")
-            print(f"PUTS:{self.stats['put']}")
-            print(f"Errors:{self.stats['errors']}")
+            print(f"total clients:{self.status['total_clients']}")
+            print(f"total operations:{self.status['total_operations']}")
+            print(f"READS:{self.status['read']}")
+            print(f"GETS:{self.status['get']}")
+            print(f"PUTS:{self.status['put']}")
+            print(f"Errors:{self.status['errors']}")
             print("======================\n")
