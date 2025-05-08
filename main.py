8698fc4903d388d125bdef4f607462fc3fbcc527
octopus1223 <2168487918@qq.com>
2025-05-08 11:29:36 +0800

    commit

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sys
+from server import TupleSpaceServer
+from client import TupleSpaceClient
+
+
 if __name__ == "__main__":
     if len(sys.argv) == 2 and sys.argv[1] == "server":
         port = int(input("Enter server port(50000-59999):"))
@@ -6,4 +11,13 @@ if __name__ == "__main__":
             server.start()
         else:
             print("Port must ba between 50000 and 59999")
-            
+    elif len(sys.argv) == 4 and sys.argv[1] == "client":
+        host = sys.argv[2]
+        port = int(sys.argv[3])
+        request_file = sys.argv[4]
+        client = TupleSpaceClient(host, port, request_file)
+        client.process_requests()
+    else:
+        print("Usage:")
+        print("Server: python tuplespace.py server")
+        print("Client: python tuplespace.py client <host><port><request_file>")
